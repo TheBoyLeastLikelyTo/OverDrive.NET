d73efb81fe9a72ca749df3aaf9c17a5ed2d637dd
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2023-12-08 00:16:59 -0800

    Updated ffmpeg automation scripts

--- OverDrive.ffmpeg/MPEG2MKV.bat
+++ OverDrive.ffmpeg/MPEG2MKV.bat
@@ -1,57 +1,102 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 set "input_folder=%~1"
 
 if "%input_folder%"=="" (
     echo [ERROR] Please provide the input directory.
-    exit /b
+    exit /b 1
 )
 
 if not exist "%input_folder%" (
     echo [ERROR] Input folder does not exist.
-    exit /b
+    exit /b 1
 )
 
-if not exist "%input_folder%\*.mp3" (
+for %%F in ("%input_folder%\*.mp3") do (
+    if "%%~xF"==".mp3" (
+        set "mp3_found=true"
+        exit /b 0
+    )
+)
+
+if not defined mp3_found (
     echo [ERROR] No MP3 files found in the input folder.
-    exit /b
+    exit /b 1
 )
 
 set "output_file=%input_folder%\combined.mka"
-echo [INFO] Using output file path: '%output_file%'
+echo [MP3 PARTS] Using output file path: '%output_file%'
+
+echo. > temp.txt
+
+for %%F in ("%input_folder%\*.mp3") do echo file '%%~fF' >> temp.txt
 
 if exist "%output_file%" (
     del "%output_file%"
 )
 
-set "temp_list=temp.txt"
+echo [MP3 PARTS] Writing MP3 parts to new file, '%output_file%'
+ffmpeg -f concat -safe 0 -i temp.txt -c copy "%output_file%" -hide_banner -loglevel error
 
-for %%F in ("%input_folder%\*.mp3") do echo file '%%~fF' >> %temp_list%
-
-echo [INFO] Writing MP3 parts to new file, '%output_file%'
-ffmpeg -f concat -safe 0 -i %temp_list% -c copy %output_file% -hide_banner -loglevel error
-
-del %temp_list%
+del temp.txt
 
 if exist "%output_file%" (
-    echo [INFO] All MP3 files from '%input_folder%' combined into '%output_file%'.
+    echo [MP3 PARTS] All MP3 files from '%input_folder%' combined into '%output_file%'.
+) else (
+    echo [ERROR] Error combining MP3 parts to '%output_file%'.
 )
 
 set "chapters_file=%input_folder%\chapters.txt"
-echo [INFO] Searching for chapters file: %chapters_file%
-
+echo [CHAPTERS] Searching for chapters file: %chapters_file%
 set "output_file_with_chapters=%input_folder%\chaptered.mka"
 
 if exist "%chapters_file%" (
-    echo [INFO] Chapters file found, writing chapters to new file, '%output_file_with_chapters%'
-    ffmpeg -i %output_file% -i %chapters_file% -map_metadata 1 -codec copy %output_file_with_chapters% -hide_banner -loglevel error
+    if exist "%output_file_with_chapters%" (
+        del "%output_file_with_chapters%"
+    )
+
+    echo [CHAPTERS] Chapters file found, writing chapters to new file, '%output_file_with_chapters%'
+    ffmpeg -i "%output_file%" -i "%chapters_file%" -map_metadata 1 -codec copy "%output_file_with_chapters%" -hide_banner -loglevel error
+
+    if exist "%output_file_with_chapters%" (
+        del "%output_file%"
+        echo [CHAPTERS] Chapters written to '%output_file_with_chapters%' successfully, original file deleted.
+        set "output_file=%output_file_with_chapters%"
+    ) else (
+        echo [ERROR] Error writing chaptered version of '%output_file%'.
+    )
 ) else (
-    exit /b
+    echo [CHAPTERS] No chapters file found at %chapters_file%
 )
 
-if exist "%output_file_with_chapters%" (
-    del "%output_file%"
-    echo [INFO] Chapters written to %output_file_with_chapters% successfully, original file deleted.
-    exit /b
+set "cover_file=%input_folder%\cover.jpg"
+echo [COVER] Searching for cover file: %cover_file%
+set "output_file_with_cover=%input_folder%\covered.mka"
+
+if exist "%cover_file%" (
+    if exist "%output_file_with_cover%" (
+        del "%output_file_with_cover%"
+    )
+
+    echo [COVER] Cover file found, writing cover to %output_file%
+    ffmpeg -i "%output_file%" -i "%cover_file%" -map 0 -map 1 -c copy -metadata:s:v filename="cover.jpg" -metadata:s:v mimetype="image/jpeg" "%output_file_with_cover%" -hide_banner -loglevel error
+
+    if exist "%output_file_with_cover%" (
+        del "%output_file%"
+        echo [COVER] Cover written to '%output_file_with_cover%' successfully, original file deleted.
+        set "output_file=%output_file_with_cover%"
+    ) else (
+        echo [ERROR] Error writing cover version of '%output_file%'.
+    )
+) else (
+    echo [COVER] No cover file found at '%cover_file%'
 )
+
+if exist "%output_file%" (
+    echo [INFO] '%output_file%' written successfully!
+) else (
+    echo [ERROR] Error writing '%output_file%'
+)
+
+exit /b
--- OverDrive.ffmpeg/MPEG2MKV.ps1
+++ OverDrive.ffmpeg/MPEG2MKV.ps1
@@ -3,56 +3,111 @@ param (
 )
 
 if (-not $input_folder) {
-    Write-Host "[ERROR] Please provide the input directory."
+    Write-Host "[ERROR] Please provide the input directory." -ForegroundColor Red
     exit
 }
 
 if (-not (Test-Path -Path $input_folder -PathType Container)) {
-    Write-Host "[ERROR] Input folder does not exist."
+    Write-Host "[ERROR] Input folder does not exist." -ForegroundColor Red
     exit
 }
 
 if (-not (Test-Path -Path "$input_folder\*.mp3")) {
-    Write-Host "[ERROR] No MP3 files found in the input folder."
+    Write-Host "[ERROR] No MP3 files found in the input folder." -ForegroundColor Red
     exit
 }
 
 $output_file = "$input_folder\combined.mka"
-Write-Host "[INFO] Using output file path: '$output_file'"
-
-if (Test-Path -Path $output_file) {
-    Remove-Item $output_file
-}
+Write-Host "[MP3 PARTS] Using output file path: '$output_file'" -ForegroundColor Cyan
 
 $temp_list = "temp.txt"
 
 Get-ChildItem -Path "$input_folder\*.mp3" | ForEach-Object { "file '$($_.FullName)'" } | Set-Content -Path $temp_list
 
-Write-Host "[INFO] Writing MP3 parts to new file, '$output_file'"
+if (Test-Path -Path $output_file) {
+    Remove-Item $output_file
+}
+
+Write-Host "[MP3 PARTS] Writing MP3 parts to new file, '$output_file'" -ForegroundColor Cyan
 .\ffmpeg -f concat -safe 0 -i $temp_list -c copy $output_file -hide_banner -loglevel error
 
 Remove-Item $temp_list
 
 if (Test-Path -Path $output_file) {
-    Write-Host "[INFO] All MP3 files from '$input_folder' combined into '$output_file'."
+    Write-Host "[MP3 PARTS] All MP3 files from '$input_folder' combined into '$output_file'." -ForegroundColor Green
+}
+else {
+    Write-Host "[ERROR] Error combining MP3 parts to '$output_file'" -ForegroundColor Red
 }
 
 $chapters_file = "$input_folder\chapters.txt"
-Write-Host "[INFO] Searching for chapters file: $chapters_file"
-
+Write-Host "[CHAPTERS] Searching for chapters file: $chapters_file" -ForegroundColor Cyan
 $output_file_with_chapters = "$input_folder\chaptered.mka"
 
+# Check whether the user has a chapters file in the book directory
 if (Test-Path -Path $chapters_file) {
-    Write-Host "[INFO] Chapters file found, writing chapters to new file, '$output_file_with_chapters'"
+    
+    if (Test-Path -Path $output_file_with_chapters) {
+        # If a chaptered mka has already been written, delete it
+        Remove-Item $output_file_with_chapters
+    }
+    
+    Write-Host "[CHAPTERS] Chapters file found, writing chapters to new file, '$output_file_with_chapters'" -ForegroundColor Cyan
     .\ffmpeg -i $output_file -i $chapters_file -map_metadata 1 -codec copy $output_file_with_chapters -hide_banner -loglevel error
+
+    # Ensure ffmpeg saved a chapterized mka file
+    if (Test-Path -Path $output_file_with_chapters) {
+        # If a mka with chapters exists:
+
+        Remove-Item $output_file # Delete the original mka
+        Write-Host "[CHAPTERS] Chapters written to '$output_file_with_chapters' successfully, original file deleted." -ForegroundColor Green
+        $output_file = $output_file_with_chapters # Update current final mka file
+    }
+    else {
+        Write-Host "[ERROR] Error writing chaptered version of '$output_file'" -ForegroundColor Red
+    }
 }
-else
-{
-    exit
+else {
+    Write-Host "[CHAPTERS] No chapters file found at $chapters_file" -ForegroundColor Red
 }
 
-if (Test-Path -Path $output_file_with_chapters) {
-    Remove-Item $output_file
-    Write-Host "[INFO] Chapters written to $output_file_with_chapters successfully, original file deleted."
-    exit
+$cover_file = "$input_folder\cover.jpg"
+Write-Host "[COVER] Searching for cover file: $cover_file" -ForegroundColor Cyan
+$output_file_with_cover = "$input_folder\covered.mka"
+
+# Check whether the user has a cover file in the book directory
+if (Test-Path -Path $cover_file) {
+    
+    if (Test-Path -Path $output_file_with_cover) {
+        # If a covered mka file has already been written, delete it
+        Remove-Item $output_file_with_cover
+    }
+    
+    Write-Host "[COVER] Cover file found, writing cover to $output_file" -ForegroundColor Cyan
+    .\ffmpeg -i $output_file -i $cover_file -map 0 -map 1 -c copy -metadata:s:v filename="cover.jpg" -metadata:s:v mimetype="image/jpeg" $output_file_with_cover -hide_banner -loglevel error
+
+    # Ensure ffmpeg saved a covered mka file
+    if (Test-Path -Path $output_file_with_cover) {
+        # If a mka with a cover exists:
+        
+        Remove-Item $output_file # Delete the original mka
+        Write-Host "[COVER] Cover written to '$output_file_with_cover' successfully, original file deleted." -ForegroundColor Green
+        $output_file = $output_file_with_cover # Update the current final mka file
+    }
+    else {
+        Write-Host "[ERROR] Error writing cover version of '$output_file'" -ForegroundColor Red
+    }
 }
+else {
+    Write-Host "[COVER] No cover file found at '$cover_file'" -ForegroundColor Red
+}
+
+# Ensure a final file has been written
+if (Test-Path -Path $output_file) {
+    Write-Host "[INFO] '$output_file' written successfully!" -ForegroundColor Green
+}
+else {
+    Write-Host "[ERROR] Error writing '$output_file'" -ForegroundColor Red
+}
+
+exit
