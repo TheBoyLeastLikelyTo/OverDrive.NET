bafdd0691b9b2ba0984306f2f1bd44290aa0d890
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2023-12-08 23:39:12 -0800

    Introduce ResizeCover

---
+++ OverDrive.ResizeCover/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Example usage: CoverArtToSquare("path_to_your_image.jpg");
+        if (args.Length != 1)
+        {
+            Console.WriteLine("[ERROR] Please provide the path to the cover art JPEG.");
+            return;
+        }
+
+        string imagePath = args[0];
+
+        if (!File.Exists(imagePath))
+        {
+            Console.WriteLine($"[ERROR] The provided file does not exist");
+            return;
+        }
+
+        if (Environment.OSVersion.Version < new Version(6, 1))
+        {
+            Console.WriteLine("[ERROR] This program requires Windows 7 or later to run.");
+            return;
+        }
+
+        try
+        {
+            Image coverImage = CoverArtToSquare(imagePath); // Get the stretched image
+            coverImage.Save(imagePath, ImageFormat.Jpeg); // Save the stretched image
+            Console.WriteLine($"[INFO] Image stretched and saved to: {imagePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("[ERROR] An error occurred: " + ex.Message);
+        }
+
+        return;
+    }
+
+    static Image CoverArtToSquare(string imagePath)
+    {
+        // Load the image from the specified path
+        using Image image = Image.FromFile(imagePath);
+        
+        if (image.Width == image.Height)
+        {
+            throw new Exception("Image already square!");
+        }
+        
+        int maxSize = Math.Max(image.Width, image.Height);
+
+        // Calculate dimensions to fit the original image into the square frame
+        int width = image.Width * maxSize / Math.Max(image.Width, image.Height);
+        int height = image.Height * maxSize / Math.Max(image.Width, image.Height);
+
+        // Create a new square bitmap
+        Bitmap squareImage = new Bitmap(maxSize, maxSize);
+
+        using (Graphics graphic = Graphics.FromImage(squareImage))
+        {
+            // Set interpolation mode to Bicubic
+            graphic.InterpolationMode = InterpolationMode.High;
+
+            // Draw the stretched image onto the square canvas
+            graphic.DrawImage(image, 0, 0, maxSize, maxSize);
+        }
+
+        return squareImage;
+    }
+}
