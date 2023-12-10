a731d3eb61019c5ce73e27dc7c6d3d746fc55dbf
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2023-12-10 01:17:27 -0800

    Vaguely improved

--- OverDrive.ffmpeg/Single.ps1
+++ OverDrive.ffmpeg/Single.ps1
@@ -1,42 +1,24 @@
-# Function to check if the directory exists and contains MP3 files
 function ValidateInputDirectory($input_folder) {
-    if (-not $input_folder) {
-        Write-Host "[ERROR] Please provide the input directory." -ForegroundColor Red
-        exit
-    }
-
-    if (-not (Test-Path -Path $input_folder -PathType Container)) {
-        Write-Host "[ERROR] Input folder does not exist: $input_folder" -ForegroundColor Red
-        exit
-    }
-
-    if (-not (Test-Path -Path "$input_folder\*.mp3")) {
-        Write-Host "[ERROR] No MP3 files found in the input folder." -ForegroundColor Red
+    if (-not $input_folder -or -not (Test-Path -Path $input_folder -PathType Container) -or -not (Test-Path -Path "$input_folder\*.mp3")) {
+        Write-Host "[ERROR] Invalid input folder or no MP3 files found." -ForegroundColor Red
         exit
     }
 }
 
-# Function to extract author and title from directory name
 function ParseDirectoryName($input_folder) {
-    $inputDirectory = Split-Path -Path $input_folder -Leaf
-    $splitValues = $inputDirectory -split ' - ' 
-
-    if ($splitValues.Count -eq 2) {
-        return $splitValues[0], $splitValues[1]
-    } else {
-        Write-Host "[ERROR] Invalid directory name format. Please make sure it follows the 'Author - Title' format." -ForegroundColor Red
+    $splitValues = (Split-Path -Path $input_folder -Leaf) -split ' - '
+    if ($splitValues.Count -ne 2) {
+        Write-Host "[ERROR] Invalid directory name format." -ForegroundColor Red
         exit
     }
+    return $splitValues[0], $splitValues[1]
 }
 
-# Main script execution
 $input_folder = $args[0]
 
-ValidateInputDirectory($input_folder)
+ValidateInputDirectory $input_folder
 
-$author, $title = ParseDirectoryName($input_folder)
-Write-Host "[INFO] Author: $author"
-Write-Host "[INFO] Title: $title"
+$author, $title = ParseDirectoryName $input_folder
 
 $output_file = Join-Path -Path $input_folder -ChildPath "combined.mka"
 
@@ -46,65 +28,46 @@ Get-ChildItem -Path "$input_folder\*.mp3" | ForEach-Object { "file '$($_.FullNam
 $cover_file = Join-Path -Path $input_folder -ChildPath "cover.jpg"
 $chapters_file = Join-Path -Path $input_folder -ChildPath "chapters.txt"
 
-if (Test-Path -Path $output_file)
-{
+if (Test-Path -Path $output_file) {
     Remove-Item $output_file
 }
 
-$fullCommand = ""
-$midCommand = ""
-$commandStart = ".\ffmpeg -f concat -safe 0 -i `"$temp_list`" "
-$commandChapters = "-i `"$chapters_file`" "
 $commandMeta = "-metadata title=`"$title`" -metadata artist=`"$author`" -metadata album=`"$title`""
 $commandCover = " -attach `"$cover_file`" -metadata:s:t mimetype=image/jpeg -metadata:s:t filename=cover.jpg"
-$commandEnd = " -c copy `"$output_file`" -hide_banner -loglevel error"
+$commandChapters = "-i `"$chapters_file`" "
 
-if ((-not (Test-Path -Path $cover_file)) -and (-not (Test-Path -Path $chapters_file)))
-{
-    # No Cover or Chapters
-    Write-Host "[INFO] No Chapters or Cover found." -ForegroundColor Cyan
-    $midCommand = $commandMeta
-}
-elseif ((Test-Path -Path $cover_file) -and (-not (Test-Path -Path $chapters_file)))
-{
-    # Cover, but no Chapters
-    Write-Host "[INFO] Cover found, no Chapters found." -ForegroundColor Cyan
-    $midCommand = $commandMeta + $commandCover
-}
-elseif ((Test-Path -Path $chapters_file) -and (-not (Test-Path -Path $cover_file)))
-{
-    # Chapters, but no Cover
-    Write-Host "[INFO] Chapters found, no Cover found." -ForegroundColor Cyan
-    $midCommand = $commandChapters + $commandMeta
-}
-elseif ((Test-Path -Path $chapters_file) -and (Test-Path -Path $cover_file))
-{
-    # Cover and Chapters
-    Write-Host "[INFO] Cover and Chapters found." -ForegroundColor Cyan
-    $midCommand = $commandChapters + $commandMeta + $commandCover
-}
-else
-{
-    <# Action when all if and elseif conditions are false #>
+$midCommand = switch ($true) {
+    { (-not (Test-Path -Path $cover_file)) -and (-not (Test-Path -Path $chapters_file)) } {
+        Write-Host "[INFO] No Chapters or Cover found." -ForegroundColor Yellow
+        $commandMeta
+    }
+    { (Test-Path -Path $cover_file) -and (-not (Test-Path -Path $chapters_file)) } {
+        Write-Host "[INFO] Cover found, no Chapters found." -ForegroundColor Yellow
+        $commandMeta + $commandCover
+    }
+    { (Test-Path -Path $chapters_file) -and (-not (Test-Path -Path $cover_file)) } {
+        Write-Host "[INFO] Chapters found, no Cover found." -ForegroundColor Yellow
+        $commandChapters + $commandMeta
+    }
+    { (Test-Path -Path $chapters_file) -and (Test-Path -Path $cover_file) } {
+        Write-Host "[INFO] Cover and Chapters found." -ForegroundColor Cyan
+        $commandChapters + $commandMeta + $commandCover
+    }
+    default { "" }
 }
 
-$fullCommand = $commandStart + $midCommand + $commandEnd
+$fullCommand = ".\ffmpeg -f concat -safe 0 -i `"$temp_list`" $midCommand -c copy `"$output_file`" -hide_banner -loglevel error"
 
-#Write-Host $fullCommand
-
-Write-Host "[INFO] Combining..." -ForegroundColor DarkYellow
+Write-Host "[INFO] Combining..." -ForegroundColor Cyan
 Invoke-Expression $fullCommand
 
 Remove-Item $temp_list
 
-if (Test-Path -Path $output_file)
-{
+if (Test-Path -Path $output_file) {
     $new_output = "$input_folder\$title.mka"
     Move-Item -Path $output_file -Destination $new_output
     Write-Host "[INFO] Successfully combined '$title' by '$author'" -ForegroundColor Green
-}
-else
-{
+} else {
     Write-Host "[ERROR] Failure combining '$title' by '$author'" -ForegroundColor Red
 }
 
