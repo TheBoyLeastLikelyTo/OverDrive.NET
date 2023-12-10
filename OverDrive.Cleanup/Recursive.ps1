a4eea21822c0535b6a907fd0a712e2f51b8d4eca
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2023-12-10 12:01:12 -0800

    Introduce OverDrive.Cleanup

---
+++ OverDrive.Cleanup/Recursive.ps1
@@ -0,0 +1,26 @@
+﻿# Main script execution
+$parentDirectory = $args[0]
+
+# Check if the parent directory exists
+if (-not (Test-Path $parentDirectory -PathType Container)) {
+    Write-Host "[ERROR] The specified directory does not exist."
+    exit
+}
+
+# Get a list of subdirectories in the parent directory
+$Books = Get-ChildItem -Path $parentDirectory -Directory
+
+# Loop through each subdirectory and execute the script with the subdirectory path
+foreach ($Book in $Books) {
+    $Path = $Book.FullName
+    Write-Host "[INFO] Combining book in subdirectory: $Path"
+    .\Single.ps1 $Path
+}
+
+# Delete *.odm files
+$odmFiles = Get-ChildItem -Path $parentDirectory -Filter "*.odm"
+foreach ($odm in $odmFiles) {
+    $Path = $odm.FullName
+    Write-Host "[INFO] Deleted ODM file: $Path" -ForegroundColor Green
+    Remove-Item $Path -Force
+}
---
+++ OverDrive.Cleanup/Single.ps1
@@ -0,0 +1,54 @@
+﻿# Main script execution
+$parentDirectory = $args[0]
+
+# Check if the parent directory exists
+if (-not (Test-Path $parentDirectory -PathType Container)) {
+    Write-Host "[ERROR] The specified directory does not exist." -ForegroundColor Red
+    exit
+}
+
+# Check for *.mka files
+$mkaFiles = Get-ChildItem -Path $parentDirectory -Filter "*.mka"
+if (-not $mkaFiles) {
+    Write-Host "[ERROR] No *.mka files found in the directory. Exiting." -ForegroundColor Red
+    exit
+}
+
+# Delete cover.jpg file
+$coverFile = Join-Path -Path $parentDirectory -ChildPath "cover.jpg"
+if (Test-Path $coverFile) {
+    Remove-Item $coverFile -Force
+    Write-Host "[INFO] Deleted cover.jpg" -ForegroundColor Green
+}
+
+# Delete *.license files
+$licenseFiles = Get-ChildItem -Path $parentDirectory -Filter "*.license"
+foreach ($license in $licenseFiles) {
+    $Path = $license.FullName
+    Write-Host "[INFO] Deleted license file: $Path" -ForegroundColor Green
+    Remove-Item $Path -Force
+}
+
+# Delete *.metadata files
+$metadataFiles = Get-ChildItem -Path $parentDirectory -Filter "*.metadata"
+foreach ($metadata in $metadataFiles) {
+    $Path = $metadata.FullName
+    Write-Host "[INFO] Deleted metadata file: $Path" -ForegroundColor Green
+    Remove-Item $Path -Force
+}
+
+# Delete *.mp3 files
+$mp3Files = Get-ChildItem -Path $parentDirectory -Filter "*.mp3"
+foreach ($mp3 in $mp3Files) {
+    $Path = $mp3.FullName
+    Write-Host "[INFO] Deleted MP3 file: $Path" -ForegroundColor Green
+    Remove-Item $Path -Force
+}
+
+# Delete files with "chapter" in the name
+$chapterFiles = Get-ChildItem -Path $parentDirectory | Where-Object { $_.Name -like '*chapter*' }
+foreach ($chapter in $chapterFiles) {
+    $Path = $chapter.FullName
+    Write-Host "[INFO] Deleted chapter file: $Path" -ForegroundColor Green
+    Remove-Item $Path -Force
+}
